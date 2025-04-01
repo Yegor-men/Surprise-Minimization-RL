48d7cafa31a2a662309f7af88d15a0ef5ff7b3f6
Yegor Men <yegormenovchshikov@gmail.com>
2025-04-01 17:34:47 +1100

    plan on making a rolling history window

--- ver2/model.py
+++ ver2/model.py
@@ -1,9 +1,97 @@
 import torch
 from torch import nn
 import torch.nn.functional as F
+from torch import distributions as dist
 
 torch.manual_seed(0)
 torch.cuda.manual_seed_all(0)
+
+from collections import deque
+
+
+class FixedSizeQueue:
+    def __init__(self, max_len: int, input_shape: tuple):
+        self.input_shape = input_shape
+        self.queue = deque([torch.zeros(self.input_shape) for _ in range(max_len)], maxlen=max_len)
+
+    def return_queue_as_batched_tensor(self):
+        return torch.stack(list(self.queue), dim=0)
+
+    def get_oldest(self):
+        return self.queue[0]
+
+    def add_element(self, element: torch.tensor):
+        self.queue.append(element)
+
+
+class ReinforceLoss(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, logprob, is_touching: bool):
+        reward = -1 if is_touching else 1
+        loss = reward * (-logprob)
+        return loss
+
+
+class ResolutionResizer(nn.Module):
+    def __init__(self, height, width, mode='interp'):
+        super().__init__()
+        self.interp_mode = "bicubic"
+        self.target_height = height
+        self.target_width = width
+        self.mode = mode
+
+    def forward(self, x):
+        if self.mode == 'avg':
+            return F.adaptive_avg_pool2d(x, (self.target_height, self.target_width))
+        elif self.mode == 'interp':
+            return F.interpolate(x, size=(self.target_height, self.target_width), mode=self.interp_mode,
+                                 align_corners=False)
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+class Model(nn.Module):
+    def __init__(self, image_history_length, h_c_size, compressed_image_size, input_shape: tuple):
+        super().__init__()
+
+        self.image_history = FixedSizeQueue(image_history_length, input_shape=input_shape)
+
+        self.h = torch.zeros(1, h_c_size)
+        self.c = torch.zeros(1, h_c_size)
+        self.lstm = nn.LSTM(input_size=compressed_image_size, hidden_size=h_c_size)
+
+        self.image_encoder = nn.Sequential(
+            nn.LazyLinear(out_features=compressed_image_size)
+        )
+
+        self.choice_maker = nn.Sequential(
+            nn.LazyLinear(out_features=1)
+        )
+
+    def update_hidden_states(self):
+        oldest_image = self.image_history.get_oldest()
+        compressed_image = self.image_encoder(oldest_image)
+        _, (n_h, n_c) = self.lstm(compressed_image, (self.h, self.c))
+        self.h, self.c = n_h, n_c
+
+    def forward(self, new_image):
+        self.image_history.add_element(new_image)
+        images = self.image_history.return_queue_as_batched_tensor()
+        compressed_images = self.image_encoder(images)
+        temp_h, temp_c = self.h, self.c
+        for i in range(compressed_images.size(0)):
+            _, (temp_h, temp_c) = self.lstm(compressed_images[i], (temp_h, temp_c))
+        distribution_logits = self.choice_maker(temp_h)
+        distribution = dist.Bernoulli(logits=distribution_logits)
+        choice = distribution.sample()
+        logprob = distribution.log_prob(choice)
+
+        return choice, logprob
+
+
+# ----------------------------------------------------------------------------------------------------------------------
 
 
 class VAELoss(nn.Module):
@@ -15,23 +103,22 @@ class VAELoss(nn.Module):
         return recon_loss
 
 
-
-
 import numpy as np
 import cv2
+
 
 class CNNOneToOne(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, stride, dilation):
         super().__init__()
         self.activation = nn.LeakyReLU()
-        self.effective_width = kernel_size + (kernel_size - 1) * (dilation-1)
+        self.effective_width = kernel_size + (kernel_size - 1) * (dilation - 1)
         print(self.effective_width)
         self.cnn = nn.Conv2d(
             in_channels=in_channels,
             out_channels=out_channels,
             kernel_size=kernel_size,
             stride=stride,
-            padding=(self.effective_width -1)//2,
+            padding=(self.effective_width - 1) // 2,
             dilation=dilation,
         )
 
@@ -39,6 +126,7 @@ class CNNOneToOne(nn.Module):
         residual = self.activation(self.cnn(image))
         # residual = residual + image
         return residual
+
 
 class CNNHalver(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size):
@@ -49,11 +137,12 @@ class CNNHalver(nn.Module):
             out_channels=out_channels,
             kernel_size=kernel_size,
             stride=2,
-            padding=(kernel_size//2)-1,
+            padding=(kernel_size // 2) - 1,
         )
 
     def forward(self, image):
         return self.activation(self.cnn(image))
+
 
 class CNNDoubler(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size):
@@ -64,25 +153,11 @@ class CNNDoubler(nn.Module):
             out_channels=out_channels,
             kernel_size=kernel_size,
             stride=2,
-            padding=(kernel_size//2)-1,
+            padding=(kernel_size // 2) - 1,
         )
+
     def forward(self, image):
         return self.activation(self.cnn(image))
-
-
-class ResolutionResizer(nn.Module):
-    def __init__(self, height, width, mode='interp'):
-        super().__init__()
-        self.interp_mode="bicubic"
-        self.target_height = height
-        self.target_width = width
-        self.mode = mode
-
-    def forward(self, x):
-        if self.mode == 'avg':
-            return F.adaptive_avg_pool2d(x, (self.target_height, self.target_width))
-        elif self.mode == 'interp':
-            return F.interpolate(x, size=(self.target_height, self.target_width), mode=self.interp_mode, align_corners=False)
 
 
 class Model(nn.Module):
@@ -104,9 +179,9 @@ class Model(nn.Module):
         )
         self.image_latent = nn.Sequential(
             nn.Flatten(),
-            nn.LazyLinear(image_latent_size*4),
+            nn.LazyLinear(image_latent_size * 4),
             nn.LeakyReLU(),
-            nn.LazyLinear(image_latent_size*2),
+            nn.LazyLinear(image_latent_size * 2),
             nn.LeakyReLU(),
             nn.LazyLinear(image_latent_size),
         )
@@ -172,14 +247,3 @@ class Decoder(nn.Module):
         n_o = dist.rsample()
         self.o_o = n_o
         return self.o_o
-
-class RewardFunction(nn.Module):
-    def __init__(self):
-        super().__init__()
-
-    def forward(self, vae_loss, s_factor, is_touching, is_between):
-        reward_value = -float(is_touching) * 10 + float(is_between) * 10
-        reward = torch.tensor([reward_value])
-        exp_reward = torch.exp(-reward).to("cuda")
-        loss = vae_loss + s_factor + exp_reward
-        return loss
